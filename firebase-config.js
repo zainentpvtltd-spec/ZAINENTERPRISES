ebfa07b384250051eefbd0baed06889f7572d611
zainentpvtltd-spec <zainent.pvt.ltd@gmail.com>
2026-05-23 23:39:10 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window._firebaseConfig = {
+    apiKey: "AIzaSyAndxKI8yv2GO3RTrjg0F8xOIYeHeiHdk4",
+    authDomain: "zain-enterprises-d781d.firebaseapp.com",
+    projectId: "zain-enterprises-d781d",
+    storageBucket: "zain-enterprises-d781d.firebasestorage.app",
+    messagingSenderId: "938488856450",
+    appId: "1:938488856450:web:89c79eec2f942b557f999b"
+};
